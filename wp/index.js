3f52a1dcb2e853ae8539543b882816a5563c9165
Chance Strickland <hi@chancedigital.io>
2019-06-29 11:24:56 -0700

    Fix parens spacing rules

--- wp/index.js
+++ wp/index.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: '../index.js',
   rules: {
     indentation: ['tab', { ignore: ['value', 'param'] }],
+    'function-parentheses-space-inside': 'always-single-line',
+    'media-feature-parentheses-space-inside': 'always',
+    'selector-pseudo-class-parentheses-space-inside': 'always',
   },
 };
